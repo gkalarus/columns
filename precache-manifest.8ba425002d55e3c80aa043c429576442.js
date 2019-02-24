295e8dbd8f93fa7fd3f3541c446cee8cb3d0bb5e
Grzegorz <kalarus.g@gmail.com>
2019-02-24 23:58:43 +0100

    Updates

---
+++ precache-manifest.8ba425002d55e3c80aa043c429576442.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e6ee599492be8bca407",
+    "url": "/columns/static/js/runtime~main.8e6ee599.js"
+  },
+  {
+    "revision": "e1dde0e96c7cca346d46",
+    "url": "/columns/static/js/main.e1dde0e9.chunk.js"
+  },
+  {
+    "revision": "a080abdb6f85b3109260",
+    "url": "/columns/static/js/2.a080abdb.chunk.js"
+  },
+  {
+    "revision": "e1dde0e96c7cca346d46",
+    "url": "/columns/static/css/main.811b69a8.chunk.css"
+  },
+  {
+    "revision": "6d1ac5c2a7384d9ea371b340388078b5",
+    "url": "/columns/index.html"
+  }
+];
